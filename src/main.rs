d9f9c417f83ae2a7b3473c9782ea046c00cf4e26
Igor Raits <ignatenkobrain@fedoraproject.org>
2020-06-06 13:53:55 +0200

    Switch from deprecated failure to anyhow
    As well as to the Rust 2018.
    
    Signed-off-by: Igor Raits <ignatenkobrain@fedoraproject.org>

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,40 @@
 /* SPDX-License-Identifier: MIT */
 
-#[macro_use]
-extern crate failure;
-extern crate ini;
-
-use failure::Error;
+use anyhow::{anyhow, Context, Result};
 use ini::Ini;
 use std::borrow::Cow;
 use std::env;
-use std::fmt;
 use std::fs;
 use std::io::{prelude::*, BufReader};
 use std::iter::FromIterator;
 use std::os::unix::fs::symlink;
 use std::path::{self, Path, PathBuf};
 use std::process::{Command, Stdio};
-use std::result;
 
-pub trait ResultExt<T, E>: failure::ResultExt<T, E>
-where
-    E: fmt::Display,
-{
-    fn with_path<P: AsRef<Path>>(self, path: P) -> result::Result<T, failure::Context<String>>
-    where
-        Self: Sized,
-    {
-        self.with_context(|e| format!("{}: {}", path.as_ref().display(), e))
-    }
-}
-
-impl<T, E: fmt::Display> ResultExt<T, E> for result::Result<T, E> where
-    result::Result<T, E>: failure::ResultExt<T, E>
-{}
-
-fn make_parent(of: &Path) -> Result<(), Error> {
-    let parent = of.parent()
-        .ok_or_else(|| format_err!("Couldn't get parent of {}", of.display()))?;
+fn make_parent(of: &Path) -> Result<()> {
+    let parent = of
+        .parent()
+        .ok_or_else(|| anyhow!("Couldn't get parent of {}", of.display()))?;
     fs::create_dir_all(&parent)?;
     Ok(())
 }
 
-fn make_symlink(dst: &str, src: &Path) -> Result<(), Error> {
+fn make_symlink(dst: &str, src: &Path) -> Result<()> {
     make_parent(src)?;
-    symlink(dst, src).with_path(src)?;
+    symlink(dst, src).with_context(|| {
+        format!(
+            "Failed to create symlink at {} (pointing to {})",
+            dst,
+            src.display()
+        )
+    })?;
     Ok(())
 }
 
-fn virtualization_container() -> Result<bool, Error> {
+fn virtualization_container() -> Result<bool> {
     match Command::new("systemd-detect-virt").arg("--container").stdout(Stdio::null()).status() {
         Ok(status) => Ok(status.success()),
-        Err(e) => Err(format_err!("systemd-detect-virt call failed: {}", e)),
+        Err(e) => Err(anyhow!("systemd-detect-virt call failed: {}", e)),
     }
 }
 
@@ -106,24 +91,24 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String], root: Cow<'static, str>) -> Result<Config, Error> {
+    fn new(args: &[String], root: Cow<'static, str>) -> Result<Config> {
         let output_directory = match args.len() {
             2 | 4 => PathBuf::from(&args[1]),
-            _ => return Err(failure::err_msg("This program requires 1 or 3 arguments")),
+            _ => return Err(anyhow!("This program requires 1 or 3 arguments")),
         };
 
         let devices = Config::read_devices(&root)?;
         Ok(Config { root, output_directory, devices })
     }
 
-    fn read_devices(root: &str) -> Result<Vec<Device>, Error> {
+    fn read_devices(root: &str) -> Result<Vec<Device>> {
         let path = Path::new(root).join("etc/systemd/zram-generator.conf");
         if !path.exists() {
             println!("No configuration file found.");
             return Ok(vec![]);
         }
 
-        Result::from_iter(Ini::load_from_file(&path).with_path(&path)?.into_iter().map(|(section_name, section)| {
+        Result::from_iter(Ini::load_from_file(&path).with_context(|| format!("Failed to read configuration from {}", path.display()))?.into_iter().map(|(section_name, section)| {
             let section_name = section_name.map(Cow::Owned).unwrap_or(Cow::Borrowed("(no title)"));
 
             if !section_name.starts_with("zram") {
@@ -137,14 +122,16 @@ impl Config {
                 if val == "none" {
                     dev.memory_limit_mb = u64::max_value();
                 } else {
-                    dev.memory_limit_mb = val.parse()
-                        .map_err(|e| format_err!("Failed to parse memory-limit \"{}\": {}", val, e))?;
+                    dev.memory_limit_mb = val.parse().map_err(|e| {
+                        anyhow!("Failed to parse memory-limit \"{}\": {}", val, e)
+                    })?;
                 }
             }
 
             if let Some(val) = section.get("zram-fraction") {
-                dev.zram_fraction = val.parse()
-                    .map_err(|e| format_err!("Failed to parse zram-fraction \"{}\": {}", val, e))?;
+                dev.zram_fraction = val.parse().map_err(|e| {
+                    anyhow!("Failed to parse zram-fraction \"{}\": {}", val, e)
+                })?;
             }
 
             println!("Found configuration for {}: memory-limit={}MB zram-fraction={}",
@@ -155,7 +142,7 @@ impl Config {
     }
 }
 
-fn handle_device(config: &Config, device: &Device, memtotal_mb: f64) -> Result<bool, Error> {
+fn handle_device(config: &Config, device: &Device, memtotal_mb: f64) -> Result<bool> {
     if memtotal_mb > device.memory_limit_mb as f64 {
         println!("{}: system has too much memory ({:.1}MB), limit is {}MB, ignoring.",
                  device.name,
@@ -188,7 +175,12 @@ ExecStart=mkswap /dev/%i
         device_name = format!("dev-{}.device", device.name),
         disksize = disksize,
     );
-    fs::write(&service_path, contents).with_path(service_path)?;
+    fs::write(&service_path, contents).with_context(|| {
+        format!(
+            "Failed to write a device service into {}",
+            service_path.display()
+        )
+    })?;
 
     let swap_name = format!("dev-{}.swap", device.name);
     let swap_path = config.output_directory.join(&swap_name);
@@ -207,7 +199,12 @@ Options=pri=100
         zram_device = device.name
     );
 
-    fs::write(&swap_path, contents).with_path(swap_path)?;
+    fs::write(&swap_path, contents).with_context(|| {
+        format!(
+            "Failed to write a swap service into {}",
+            swap_path.display()
+        )
+    })?;
 
     let symlink_path = config.output_directory.join("swap.target.wants").join(&swap_name);
     let target_path = format!("../{}", swap_name);
@@ -215,7 +212,7 @@ Options=pri=100
     Ok(true)
 }
 
-fn run(config: Config) -> Result<(), Error> {
+fn run(config: Config) -> Result<()> {
     let memtotal = get_total_memory_kb(&config.root)?;
     let memtotal_mb = memtotal as f64 / 1024.;
 
@@ -232,16 +229,26 @@ fn run(config: Config) -> Result<(), Error> {
         /* We created some services, let's make sure the module is loaded */
         let modules_load_path = Path::new(&config.root[..]).join("run/modules-load.d/zram.conf");
         make_parent(&modules_load_path)?;
-        fs::write(&modules_load_path, "zram\n").with_path(modules_load_path)?;
+        fs::write(&modules_load_path, "zram\n").with_context(|| {
+            format!(
+                "Failed to write configuration for loading a module at {}",
+                modules_load_path.display()
+            )
+        })?;
     }
 
     Ok(())
 }
 
-fn get_total_memory_kb(root: &str) -> Result<u64, Error> {
+fn get_total_memory_kb(root: &str) -> Result<u64> {
     let path = Path::new(root).join("proc/meminfo");
 
-    for line in BufReader::new(fs::File::open(&path).with_path(&path)?).lines() {
+    for line in
+        BufReader::new(fs::File::open(&path).with_context(|| {
+            format!("Failed to read memory information from {}", path.display())
+        })?)
+        .lines()
+    {
         let line = line?;
         let mut fields = line.split_whitespace();
         if let Some("MemTotal:") = fields.next() {
@@ -251,5 +258,5 @@ fn get_total_memory_kb(root: &str) -> Result<u64, Error> {
         }
     }
 
-    Err(format_err!("Couldn't find MemTotal in {}", path.display()))
+    Err(anyhow!("Couldn't find MemTotal in {}", path.display()))
 }
